be3448411389ae886a00aede4ff8aa3da557747b
abdelsayedzaki-design <abdelsayedzaki@gmail.com>
2026-01-26 09:44:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
-const CACHE_NAME = 'is-app-v1';
+const CACHE_NAME = 'is-app-v2'; // غيرنا الرقم لكي يفهم المتصفح أن هناك تحديث
+const ASSETS = [
+  './',
+  'index.html',
+  'sw.js'
+];
+
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(['./', 'index.html']))
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => { if(key !== CACHE_NAME) return caches.delete(key); })
+    ))
+  );
+});
+
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => caches.match('index.html'))
+  );
 });
